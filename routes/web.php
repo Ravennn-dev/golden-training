68bb93a5e78012d12e398921404a7f1804576874
Jor Programmer <rogelio.santos@goldenarrowsolution.com>
2025-06-06 10:55:35 +0800

    add example

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,44 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test-example', function () {
+
+    $plus = '+';
+    $minus = '-';
+    $output = [];
+    $rowData = '';
+
+    for ($ctrRow = 0; $ctrRow < 5; $ctrRow++) {
+
+        for ($ctrCol = 0; $ctrCol < 5; $ctrCol++) {
+
+            if ($ctrRow % 2 == 0)
+                $rowData .= "{$plus} ";
+            else
+                $rowData .= "{$minus} ";
+        }
+
+        $output[] = trim($rowData);
+        $rowData = '';
+    }
+
+    return response()->json($output);
+});
+
 Route::get('/test-1', function () {
-    return response()->json(['test1']);
+    $output = ['test1'];
+
+    return response()->json($output);
 });
 
 Route::get('/test-2', function () {
-    return response()->json(['test2']);
+    $output = ['test2'];
+
+    return response()->json($output);
 });
 
 Route::get('/test-3', function () {
-    return response()->json(['test3']);
+    $output = ['test3'];
+
+    return response()->json($output);
 });
--- tests/Feature/ActivityTest.php
+++ tests/Feature/ActivityTest.php
@@ -4,24 +4,37 @@ use Tests\TestCase;
 
 class ActivityTest extends TestCase
 {
+    public function test_example()
+    {
+        $response = $this->get('/test-example');
+
+        $response->assertJson([
+            '+ + + + +',
+            '- - - - -',
+            '+ + + + +',
+            '- - - - -',
+            '+ + + + +',
+        ]);
+    }
+
     public function test_test1()
     {
         $response = $this->get('/test-1');
 
-        $response->assertJson(['test1']);
+        $response->assertJson([]);
     }
 
     public function test_test2()
     {
         $response = $this->get('/test-2');
 
-        $response->assertJson(['test2']);
+        $response->assertJson([]);
     }
 
     public function test_test3()
     {
         $response = $this->get('/test-3');
 
-        $response->assertJson(['test3']);
+        $response->assertJson([]);
     }
 }
